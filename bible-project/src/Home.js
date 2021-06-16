b3ed08faab8107f44714eb70c1ad9f24e8cf4611
poojanavgurukul <pooja17@navgurukul.org>
2021-06-16 22:32:20 +0530

    some Basic structure

---
+++ bible-project/src/Home.js
@@ -0,0 +1,11 @@
+import VerticalTabs from "./components/Home/Tab";
+
+const HomePage = () => {
+    return ( 
+        <div>
+            <VerticalTabs />
+        </div>
+     );
+}
+ 
+export default HomePage;
---
+++ bible-project/src/components/Home/Tab.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Box from '@material-ui/core/Box';
+import LanguageIcon from '@material-ui/icons/Language';
+import ListAltIcon from '@material-ui/icons/ListAlt';
+import LocalLibraryIcon from '@material-ui/icons/LocalLibrary';
+import ArtTrackIcon from '@material-ui/icons/ArtTrack';
+
+import { makeStyles } from '@material-ui/core/styles';
+import LanguageTable from '../Language/languageTable';
+
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>{children}</Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+    display: 'flex',
+    height: 'auto',
+  },
+  tabs: {
+    borderRight: `1px solid ${theme.palette.divider}`,
+  },
+}));
+
+export default function VerticalTabs() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.root}>
+      <Tabs
+        orientation="vertical"
+        variant="scrollable"
+        value={value}
+        onChange={handleChange}
+        aria-label="Vertical tabs example"
+        className={classes.tabs}
+      >
+        <Tab icon={<LanguageIcon />} label="Language" {...a11yProps(0)} />
+        <Tab icon={<ListAltIcon />} label="Version" {...a11yProps(1)} />
+        <Tab icon={<LocalLibraryIcon />} label="Bible" {...a11yProps(2)} />
+        <Tab icon={<ArtTrackIcon />} label="Project" {...a11yProps(3)} />
+      </Tabs>
+      <TabPanel value={value} index={0}>
+          <LanguageTable />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Item Two
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Item Three
+      </TabPanel>
+      <TabPanel value={value} index={3}>
+        Item Four
+      </TabPanel>
+    </div>
+  );
+}
