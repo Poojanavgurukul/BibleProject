e8483e95772e215e6bca625b64bbe45721a646c2
poojanavgurukul <pooja17@navgurukul.org>
2021-07-05 13:32:16 +0530

    added the dispaly the list and added delete functionality

--- todos/src/App.js
+++ todos/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import TodoList from './component/Todo/list';
 
-function App() {
+class App extends Component{
+  state = {
+    todos:[
+      { id:1,content:'Learn Redux' },
+      { id:2,content:'daily Update' }
+    ]
+  }
+  deleteTodo=(id)=>{
+    console.log(id,'I have id')
+    const todos=this.state.todos.filter((todo)=>{
+      return todo.id !== id
+    });
+    this.setState({
+      todos
+    })
+  }
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="center blue-text">Todo's</h1>
+      <div className="todo-app container">
+      <TodoList todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+    </div>
     </div>
   );
+}
 }
 
 export default App;
---
+++ todos/src/component/Todo/list.js
@@ -0,0 +1,24 @@
+const TodosList = ({todos,deleteTodo}) => {
+    const todoList = todos.length?(
+        todos.map((todo)=>(
+            <div className="collection-item" key={todo.id}>
+                <span>{todo.content}</span>
+                <button 
+                    onClick={()=>deleteTodo(todo.id)}
+                    className="btn space"
+                >
+                    Delete
+                </button>
+            </div>
+        ))
+    ): (
+        <p className="App">You have no Todo's Left </p>
+    );
+    return ( 
+        <div className="todos collection">
+            {todoList}
+        </div>
+     );
+}
+ 
+export default TodosList;
